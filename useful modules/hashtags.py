cc39f35856d0e97aac50457a34421e1b0fdf9c1e
Shepard2154 <john-shepard-2154@yandex.ru>
2022-03-05 10:49:57 +0300

    fixed some bugs, cleaned code

--- useful modules/hashtags.py
+++ useful modules/hashtags.py
@@ -2,6 +2,7 @@ import os
 import re
 
 import tweepy
+from loguru import logger
 from dotenv import load_dotenv
 from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
@@ -11,6 +12,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 load_dotenv()
+
+logger.add("main.log", format="{time}  {message}", level="DEBUG", rotation="500 MB", compression="zip", encoding='utf-8')
 
 auth = tweepy.OAuthHandler(os.getenv('CONSUMER_KEY'), os.getenv('CONSUMER_SECRET'))
 auth.set_access_token(os.getenv('ACCESS_KEY'), os.getenv('ACCESS_SECRET'))
@@ -30,7 +33,7 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
-browser = webdriver.Chrome(ChromeDriverManager().install(), seleniumwire_options=options, chrome_options=chrome_options)
+driver = webdriver.Chrome(ChromeDriverManager().install(), seleniumwire_options=options, chrome_options=chrome_options)
 
 os.environ['http_proxy'] = proxy
 os.environ['HTTP_PROXY'] = proxy
@@ -38,29 +41,33 @@ os.environ['https_proxy'] = proxy
 os.environ['HTTPS_PROXY'] = proxy
 
 
+@logger.catch
 def getting_tweets_by_hashtag(hashtag_value):
-    browser.get('https://twitter.com/search?q=%23'+hashtag_value+'&src=typed_query&f=live')
-    WebDriverWait(browser, 10).until(
-        expected_conditions.presence_of_all_elements_located((By.XPATH, "//article[@role='article']"))) 
-
+    driver.get('https://twitter.com/search?q=%23'+hashtag_value+'&src=typed_query&f=live')
+    
     while True:
-        raw_tweets = browser.find_elements(by=By.XPATH, value="//article[@role='article']")
-        print('\ncount of tweets: ', len(raw_tweets))
-        for raw_tweet in raw_tweets:
-            status_link = raw_tweet.find_element(by=By.XPATH, value=".//following::time/parent::*").get_attribute('href')
+        WebDriverWait(driver, 10).until(
+            expected_conditions.presence_of_all_elements_located((By.XPATH, "//article[@role='article']"))) 
 
-            status_id_pattern = r"status\/(\d+)"
-            status_id = re.search(status_id_pattern, status_link).group(1)
+        tweets_elements = driver.find_elements(by=By.XPATH, value="//article[@role='article']//following::time/parent::*")
+        tweets_links = list(filter(None, [element.get_attribute('href') for element in tweets_elements]))
 
-            tweet = api.get_status(id=status_id)
+        logger.debug(f"count of tweets: {len(tweets_links)}")
+        for link in tweets_links: 
+            tweet_id_pattern = r"status\/(\d+)"
+            tweet_id = re.search(tweet_id_pattern, link).group(1)
+
+            tweet = api.get_status(id=tweet_id)
             yield tweet._json
-        
-        browser.execute_script("arguments[0].scrollIntoView();", raw_tweets[-1])
-        WebDriverWait(browser, 10).until(
-            expected_conditions.presence_of_all_elements_located((By.XPATH, "//article[@role='article']")))
+
+        scroll_to(tweets_elements[-1])        
 
 
-for tweet in getting_tweets_by_hashtag('cb'):
-    print(tweet.get('user').get('screen_name'), tweet.get('id'))
+def scroll_to(element):
+    driver.execute_script("arguments[0].scrollIntoView();", element)
 
+
+if __name__ == 'main':
+    for tweet in getting_tweets_by_hashtag('cb'):
+        print(tweet.get('user').get('screen_name'), tweet.get('id'))
 
