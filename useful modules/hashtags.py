64d3631701ad179da91b7135a612702fac913d34
Shepard2154 <john-shepard-2154@yandex.ru>
2022-03-05 09:39:37 +0300

    added scroll fragment with bugs

--- useful modules/hashtags.py
+++ useful modules/hashtags.py
@@ -1,11 +1,12 @@
 import os
 import re
-import time
 
 import tweepy
 from dotenv import load_dotenv
 from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -29,7 +30,7 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
-wd = webdriver.Chrome(ChromeDriverManager().install(), seleniumwire_options=options, chrome_options=chrome_options)
+browser = webdriver.Chrome(ChromeDriverManager().install(), seleniumwire_options=options, chrome_options=chrome_options)
 
 os.environ['http_proxy'] = proxy
 os.environ['HTTP_PROXY'] = proxy
@@ -37,25 +38,29 @@ os.environ['https_proxy'] = proxy
 os.environ['HTTPS_PROXY'] = proxy
 
 
-def get_some_tweets(query):
-    some_tweets = []
+def getting_tweets_by_hashtag(hashtag_value):
+    browser.get('https://twitter.com/search?q=%23'+hashtag_value+'&src=typed_query&f=live')
+    WebDriverWait(browser, 10).until(
+        expected_conditions.presence_of_all_elements_located((By.XPATH, "//article[@role='article']"))) 
 
-    wd.get('https://twitter.com/search?q=%23'+query+'&src=typed_query')
-    time.sleep(10)
+    while True:
+        raw_tweets = browser.find_elements(by=By.XPATH, value="//article[@role='article']")
+        print('\ncount of tweets: ', len(raw_tweets))
+        for raw_tweet in raw_tweets:
+            status_link = raw_tweet.find_element(by=By.XPATH, value=".//following::time/parent::*").get_attribute('href')
 
-    raw_tweets = wd.find_elements(by=By.XPATH, value="//article[@role='article']")
-    print('count of tweets: ', len(raw_tweets))
-    for raw_tweet in raw_tweets:
-        # screen_name = raw_tweet.find_element(by=By.XPATH, value=".//following::span[contains(.,'@')]").get_attribute('innerHTML')
-        # created_at = raw_tweet.find_element(by=By.XPATH, value=".//following::time").get_attribute('datetime')
-        status_link = raw_tweet.find_element(by=By.XPATH, value=".//following::time/parent::*").get_attribute('href')
+            status_id_pattern = r"status\/(\d+)"
+            status_id = re.search(status_id_pattern, status_link).group(1)
 
-        status_id_pattern = r"status\/(\d+)"
-        status_id = re.search(status_id_pattern, status_link).group(1)
+            tweet = api.get_status(id=status_id)
+            yield tweet._json
+        
+        browser.execute_script("arguments[0].scrollIntoView();", raw_tweets[-1])
+        WebDriverWait(browser, 10).until(
+            expected_conditions.presence_of_all_elements_located((By.XPATH, "//article[@role='article']")))
 
-        tweet = api.get_status(id=status_id)
-        some_tweets.append(tweet._json)
-    return some_tweets
 
-some_tweets = get_some_tweets('cb')
-print(some_tweets, len(some_tweets))
+for tweet in getting_tweets_by_hashtag('cb'):
+    print(tweet.get('user').get('screen_name'), tweet.get('id'))
+
+
