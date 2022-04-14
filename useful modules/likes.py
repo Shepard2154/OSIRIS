cd27bb0b5ebaf31a8078a3bcc2419457e3906fb0
no1ndsme <75178009+no1ndsme@users.noreply.github.com>
2022-04-14 21:17:24 +0300

    Update likes.py

--- useful modules/likes.py
+++ useful modules/likes.py
@@ -1,5 +1,6 @@
 import tweepy
 import os
+import re
 
 proxy='http://dtiBPkKp:iX4U87RK@45.132.51.236:49044'
 os.environ['http_proxy'] = proxy 
@@ -17,9 +18,9 @@ auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 def get_likes(USER, n_posts):
-  tweet_list=api.favorites(screen_name = USER, count = n_posts)
-  usr = api.get_user(USER)
+  tweet_list=api.get_favorites(screen_name = USER, count = n_posts)
+  usr = tweepy.API.get_user(api, screen_name=USER)
   likes = []
   for i in tweet_list:
-    likes.append({'liker' : USER, 'liker_id' : usr.id ,'liked_user' : i.user.screen_name, 'liked_user_id' : i.user.id, 'tweet_text' : i.text , 'tweet_hashtags' : re.findall(r'(#\w+)', i.text), 'tweet_links' : re.findall("(?P<url>https?://[^\s]+)", i.text)})
+    likes.append({'liker' : USER, 'liker_id' : usr.id ,'liked_user' : i.user.screen_name, 'liked_user_id' : i.user.id, 'liked_post_id' : i.id, 'tweet_text' : i.text , 'tweet_hashtags' : re.findall(r'(#\w+)', i.text), 'tweet_links' : re.findall("(?P<url>https?://[^\s]+)", i.text)})
   return(likes)
